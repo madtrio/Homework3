e7894ed8c54ca0014dff5e499b66a639d4034146
madtro <anna.kozitzyna@yandex.ru>
2022-12-25 19:56:08 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task1");
+        int oneInt = 2000000000;
+        byte twoByte = 100;
+        short threeShort = 20000;
+        long fourLong = 1000000000000000000L;
+        float fiveFloat = 1.05f;
+        double sixDouble = 1.0553652356321656458546455634;
+        System.out.println("Значение переменной oneInt с типом int равно " + oneInt);
+        System.out.println("Значение переменной twoByte с типом byte равно " + twoByte);
+        System.out.println("Значение переменной threeShort с типом short равно " + threeShort);
+        System.out.println("Значение переменной fourLong с типом long равно " + fourLong);
+        System.out.println("Значение переменной fiveFloat с типом float равно " + fiveFloat);
+        System.out.println("Значение переменной sixDouble с типом double равно " + sixDouble);
+
+
     }
 }
